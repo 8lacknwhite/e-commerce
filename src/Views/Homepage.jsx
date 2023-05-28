2d9dd4051a6f5d6d77b01c11e2dc6af6435c6567
Nimit Kumar <nimitkumar2002@gmail.com>
2023-05-28 19:57:38 +0530

    first commit

---
+++ src/Views/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavigationBar from "../Components/Navbar";
+
+function Homepage() {
+    return(
+        <div>
+            <NavigationBar/>
+        </div>
+    );
+}
+
+export default Homepage;
