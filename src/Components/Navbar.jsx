0378670e4178e00560453cc83b3afacff3c6b101
Nimit Kumar <nimitkumar2002@gmail.com>
2023-05-29 20:12:06 +0530

    Navbar made and homepage started

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,34 +1,52 @@
+import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { BsFillCartFill} from "react-icons/bs";
 
-function BasicExample() {
+function NavigationBar() {
   return (
-    <Navbar bg="light" expand="lg" sticky='top'>
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <NavDropdown title="Shop" id="navbarScrollingDropdown">
+              <NavDropdown.Item href="#action3">Shop</NavDropdown.Item>
+              <NavDropdown.Item href="#action4">
                 Another action
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#action5">
+                Something else here
               </NavDropdown.Item>
             </NavDropdown>
+            <Nav.Link href="#action2">Stories</Nav.Link>
+            <Nav.Link href="#action3">About</Nav.Link>
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>{' '}
+
+          </Form>
           </Nav>
+          <Button varient="outline-success" size="sm"><BsFillCartFill/></Button>
+          <Button size="sm" variant='light'>Login</Button>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default BasicExample;
+export default NavigationBar;
--- src/Views/Homepage.jsx
+++ src/Views/Homepage.jsx
@@ -1,10 +1,59 @@
 import React from "react";
 import NavigationBar from "../Components/Navbar";
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Carousel from "react-bootstrap/Carousel";
 
 function Homepage() {
     return(
         <div>
-            <NavigationBar/>
+            <Container>
+                <NavigationBar/>
+            </Container>
+            <hr></hr>
+            <br></br>
+            <br></br>
+            <Container>
+                <h1 className="m-auto">Better Clothing for the planet</h1>
+                <br></br>
+                <p>Create screens directly in Method  or add your images from the Sketch or<br></br>Figma. You can even sync design from the cloud storage!</p>
+                <br></br>
+                <Button variant="outline-dark" size="lg">Shop All</Button>
+            </Container>
+            <Container>
+                <Carousel>
+                    <Carousel.Item>
+                        <img 
+                        className="d-block w-100"
+                        src="https://placehold.jp/400x200.png"
+                        alt="First slide"/>
+                        <Carousel.Caption>
+                            <h3>First Slide</h3>
+                            <p>This is the first slide of the carousel</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://placehold.jp/400x200.png"
+                        alt="Second slide"/>
+                        <Carousel.Caption>
+                            <h3>Second Slide</h3>
+                            <p>This is the second slide of the carousel</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="https://placehold.jp/400x200.png"
+                        alt="Third Slide"/>
+                        <Carousel.Caption>
+                            <h3>Third Slide</h3>
+                            <p>This is the third slide of the</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </Container>
         </div>
     );
 }
